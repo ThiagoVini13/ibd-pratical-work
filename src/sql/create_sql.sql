23add8e72dd4acaad00ad1804d3e2dd3665da14b
Nicolas <araujo.sampaio.nicolas@gmail.com>
2024-11-30 00:40:55 -0400

    MODIFICAÇÃO NAS TABELAS COMENTARIO GRUPO E CURTIDA GRUPO E SET NULL

--- src/sql/create_sql.sql
+++ src/sql/create_sql.sql
@@ -6,6 +6,7 @@ CREATE TABLE `usuarios` (
     `cod_usuario` INT NOT NULL AUTO_INCREMENT,
     `nome` VARCHAR(255) NOT NULL,
     `data_nascimento` DATE NOT NULL,
+    'foto_perfil' BLOB,
     `biografia` VARCHAR(255),
     `localizacao` VARCHAR(255),
     PRIMARY KEY (cod_usuario)
@@ -13,9 +14,9 @@ CREATE TABLE `usuarios` (
 
 CREATE TABLE `mensagem_usuario` (
     `cod_mensagem` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `cod_usu_remetente` INT NOT NULL,
+    `cod_usu_remetente` INT DEFAULT NULL, --Foi escolhido default NULL pois caso o usuário seja excluido, as mesagens com o destinatário permancem
     `cod_usu_destinatario` INT DEFAULT NULL,
-    `conteudo` TEXT NOT NULL,
+    `conteudo` VARCHAR(255) NOT NULL,
     `envio_data_hora` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `recebe_data_hora` DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (cod_usu_remetente) REFERENCES usuarios(cod_usuario) ON DELETE SET NULL,
@@ -28,7 +29,7 @@ CREATE TABLE `grupo` (
     `nome` VARCHAR(255) NOT NULL,
     `descricao` VARCHAR(255),
     PRIMARY KEY(cod_grupo),
-    FOREIGN key(cod_criador) REFERENCES usuarios(cod_usuario) ON DELETE SET NULL
+    FOREIGN key(cod_criador) REFERENCES usuarios(cod_usuario)
 ) Engine=InnoDB;
 
 CREATE TABLE 'categoria_grupo'(
@@ -41,11 +42,11 @@ CREATE TABLE 'categoria_grupo'(
 CREATE TABLE `mensagem_grupo` (
     `cod_msg` INT NOT NULL AUTO_INCREMENT,
     `cod_grupo` INT NOT NULL,
-    `cod_usuario` INT NOT NULL,
+    `cod_usuario` INT DEFAULT NULL, -- CASO O USUÁRIO SEJA EXCLUIDO, A MSG CONTINUA
     `conteudo` VARCHAR(255) NOT NULL,
     `data_hora` DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(cod_msg, cod_grupo, cod_usuario),
-    FOREIGN KEY(cod_grupo) REFERENCES grupo(cod_grupo) ON DELETE CASCADE,
+    FOREIGN KEY(cod_grupo) REFERENCES grupo(cod_grupo),
     FOREIGN KEY(cod_usuario) REFERENCES usuarios(cod_usuario) ON DELETE SET NULL
 ) Engine=InnoDB;
 
@@ -64,28 +65,32 @@ CREATE TABLE `postagem_grupo` (
 ) Engine=InnoDB;
 
 CREATE TABLE `comentario_grupo` (
-    `cod_grupo` INT NOT NULL,
-    `cod_usuario` INT NOT NULL,
+    `cod_grupo` INT NOT NULL, -- Caso o grupo seja excluido, todos os comentarios nas postagens são excluidas
+    `cod_usuario` INT DEFAULT NULL, --Caso o usuário seja excluido, os comentários permanecem
+    `cod_post_grupo` INT NOT NULL,
     `data_hora` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `comentario` VARCHAR(255) NOT NULL,
-    PRIMARY KEY(cod_grupo, cod_usuario, data_hora),
+    PRIMARY KEY(cod_grupo, cod_usuario, cod_post_grupo, data_hora),
     FOREIGN KEY(cod_usuario) REFERENCES usuarios(cod_usuario) ON DELETE SET NULL,
     FOREIGN KEY(cod_grupo) REFERENCES grupo(cod_grupo) ON DELETE CASCADE
+    FOREIGN KEY(cod_post_grupo) REFERENCES postagem_grupo(cod_post_grupo) ON DELETE CASCADE
 ) Engine=InnoDB;
 
 CREATE TABLE `curtida_grupo` (
     `cod_grupo` INT NOT NULL,
     `cod_usuario` INT NOT NULL,
+    `cod_post_grupo` INT NOT NULL,
     `data_hora` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(cod_grupo, cod_usuario),
+    PRIMARY KEY(cod_grupo, cod_usuario, cod_post_grupo),
     FOREIGN KEY(cod_usuario) REFERENCES usuarios(cod_usuario) ON DELETE CASCADE,
     FOREIGN KEY(cod_grupo) REFERENCES grupo(cod_grupo) ON DELETE CASCADE
+    FOREIGN KEY(cod_post_grupo) REFERENCES postagem_grupo(cod_post_grupo) ON DELETE CASCADE
 ) Engine=InnoDB;
 
 CREATE TABLE `postagem_usuario` (
     `cod_post` INT NOT NULL AUTO_INCREMENT,
     `cod_usuario` INT NOT NULL,
-    'texto' VARCHAR(1000) NOT NULL,
+    'texto' VARCHAR(256) NOT NULL,
     `imagem` MEDIUMBLOB,
     `video` LONGBLOB,
     `gif` BLOB,
